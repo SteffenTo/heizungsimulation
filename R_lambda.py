e1d26ea08006d78ecd9e97623e0a15cb4316213a
SteffenTo <52286374+SteffenTo@users.noreply.github.com>
2019-07-04 15:45:07 +0200

    Update R_lambda.py

--- R_lambda.py
+++ R_lambda.py
@@ -1,7 +1,6 @@
 class Waermewiderstand:
 
-    def __init__(self, _Lambda_glas, s_fenster, status_styropor_wand, status_styropor_boden, status_styropor_dach,
-                 s_styropor):  # s_fenster evtl == s_wand
+    def __init__(self, _Lambda_glas, s_fenster, status_styropor_wand, status_styropor_boden, status_styropor_dach, s_styropor):
         self.lambda_glas = _Lambda_glas
         self.s_fenster = s_fenster
         self.status_styropor_wand = status_styropor_wand
@@ -9,7 +8,7 @@ class Waermewiderstand:
         self.status_styropor_dach = status_styropor_dach
         self.s_styropor = s_styropor
 
-        self.dach = 60.208  # test variablen für Variablen die von der Flächenfkt. ausgegeben werden
+        self.dach = 60.208                                          # test variablen für Variablen die von der Flächenfkt. ausgegeben werden
         self.dachfenster = 0.647
         self.fenster = 1.596
         self.tuer = 1.793
@@ -17,34 +16,32 @@ class Waermewiderstand:
         self.l_wand = 35
         self.boden = 80
 
-        self.s_putz = 0.025  # Deklaration der Attribute die für Instanzen der class gelten
+        self.s_putz = 0.025                                         # Deklaration der Attribute die für Instanzen der class gelten
         self.s_mauer = 0.115
         self.s_tuer = self.s_putz + self.s_mauer
         self.lambda_putz = 0.85
         self.lambda_hochlochziegeln = 0.95
         self.lambda_styropor = 0.035
 
-        self.r_lambda_dach = ((1 / self.r_lambda_pzs(status_styropor_dach, s_styropor, self.dach)) + (
-                    1 / self.r_lambda(s_fenster, _Lambda_glas, self.dachfenster))) ** (-1)
-        self.r_lambda_k_wand = ((1 / self.r_lambda_pzs(status_styropor_wand, s_styropor,self. k_wand)) + (
-                    1 / self.r_lambda(self.s_tuer, _Lambda_glas, self.tuer)) + (
-                                            1 / self.r_lambda(s_fenster, _Lambda_glas, self.fenster))) ** (
-                                   -1)  # _Lambda_glas der Tür muss immer gleich bleiben und verändert sich nicht mit der Doppelverglasung
+        self.r_lambda_dach = ((1 / self.r_lambda_pzs(status_styropor_dach, s_styropor, self.dach)) + (1 / self.r_lambda(s_fenster, _Lambda_glas, self.dachfenster))) ** (-1)
+        
+        self.r_lambda_k_wand = ((1 / self.r_lambda_pzs(status_styropor_wand, s_styropor,self. k_wand)) + (1 / self.r_lambda(self.s_tuer, _Lambda_glas, self.tuer)) + (
+                    1 / self.r_lambda(s_fenster, _Lambda_glas, self.fenster))) ** (-1)                                  # _Lambda_glas der Tür muss immer gleich bleiben und verändert sich nicht mit der Doppelverglasung
+
         self.r_lambda_l_wand = ((1 / self.r_lambda_pzs(status_styropor_wand, s_styropor, self.l_wand)) + (
                     2 / self.r_lambda(s_fenster, _Lambda_glas, self.fenster))) ** (-1)
+
         self.r_lambda_boden = self.r_lambda_pzs(status_styropor_boden, s_styropor, self.boden)
+
         print("Der Waermewiderstand des Hauses beträgt", self.r_lambda_gesamt(_Lambda_glas, s_fenster, s_styropor),
               "K/W.")
 
-    def r_lambda(self, s, lambda_allgemein,
-                 flaeche):  # allgemeine Funktion zur Berechnung eines einzelnen Wärmewiderstandes
+    def r_lambda(self, s, lambda_allgemein, flaeche):                                                                   # allgemeine Funktion zur Berechnung eines einzelnen Wärmewiderstandes
         r_lambda = s / (lambda_allgemein * flaeche)
         return r_lambda
 
-    def r_lambda_pzs(self, status_styropor, s_styropor,
-                     flaeche):  # Berechung des Wärmewiderstandes von der oft benötigten Kombination Putz + Ziegel + Putz + evtl. Styropor
-        r_lambda_pzs = self.r_lambda(self.s_putz, self.lambda_putz, flaeche) + self.r_lambda(self.s_mauer, self.lambda_hochlochziegeln,
-                                                                                        flaeche)
+    def r_lambda_pzs(self, status_styropor, s_styropor, flaeche):                                                       # Berechung des Wärmewiderstandes von der oft benötigten Kombination Putz + Ziegel + Putz + evtl. Styropor
+        r_lambda_pzs = self.r_lambda(self.s_putz, self.lambda_putz, flaeche) + self.r_lambda(self.s_mauer, self.lambda_hochlochziegeln, flaeche)
         if status_styropor == False:
             pass
         else:
@@ -65,3 +62,9 @@ r_lambda_haus.r_lambda_gesamt(5.6, 0.004, 0.07)
 
 r_lambda_haus = Waermewiderstand(0.6, 0.044, True, True, True, 0.15)
 r_lambda_haus.r_lambda_gesamt(0.6, 0.044, 0.15)
+
+
+
+
+
+
