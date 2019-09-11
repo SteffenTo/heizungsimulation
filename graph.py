05f8aebb39eb53e05cf06e59f22186f65d56b5b4
SteffenTo <52286374+SteffenTo@users.noreply.github.com>
2019-09-11 18:37:28 +0200

    Add files via upload

---
+++ graph.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+def graph(avg_values_per_month, plot_days, waermestrom_stuendlich):
+    plt.figure(figsize=(10, 5))
+    plt.plot(range(1, 13), avg_values_per_month)
+    plt.title("Jahresverlauf")
+    plt.xlabel("Monat")
+    plt.ylabel("Durchschnittswärmestrom (in W(h))")
+
+
+    plt.figure(figsize=(10, 5))
+
+    for (day, month) in plot_days:
+        tageswaermestrom = waermestrom_stuendlich[
+                           (day - 1) * 24 + (month - 1) * 24 * 30: day * 24 + (month - 1) * 24 * 30]
+        plt.plot(tageswaermestrom, label=str(day) + "." + str(month))
+    plt.legend()
+    plt.ylabel('Wärmestrom (in W(h))')
+    plt.xlabel("Uhrzeit (in Stunden)")
+    plt.title("Tagesvergleiche")
+    plt.show()
