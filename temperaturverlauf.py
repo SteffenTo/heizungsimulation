cb9e30d7312214f4ef4fc093ca53308b5aaf16df
LucaPodi <52287213+LucaPodi@users.noreply.github.com>
2019-09-13 11:09:39 +0200

    Update temperaturverlauf.py

--- temperaturverlauf.py
+++ temperaturverlauf.py
@@ -1,5 +1,5 @@
 import math as m
-def temperature_Haus(hours, temp_diff, temp_max, temp_min, tmin):
+def temperatur_haus(hours, temp_diff, temp_max, temp_min, tmin):
     return round((1/2*temp_diff*m.sin(m.pi*1/12*(hours-6-tmin))+((temp_max-temp_min)/2)*m.sin(m.pi*1/4320*(hours-2160))+(((temp_max-temp_min)/2)+temp_min)),2)
 
 def get_temperaturverlauf(months_start, months_end, days, temp_diff, temp_max, temp_min, tmin):
@@ -9,4 +9,4 @@ def get_temperaturverlauf(months_start, months_end, days, temp_diff, temp_max, t
     
     return temperaturverlauf_haus
 
-        
+def temperatur_keller
