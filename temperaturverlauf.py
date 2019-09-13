a9507c910e81ef864807a41450b9d0ef923af63c
LucaPodi <52287213+LucaPodi@users.noreply.github.com>
2019-09-13 11:18:39 +0200

    Update temperaturverlauf.py

--- temperaturverlauf.py
+++ temperaturverlauf.py
@@ -2,7 +2,7 @@ import math as m
 def temperaturverlauf_haus(hours, temp_diff, temp_max, temp_min, tmin):
     return round((1/2*temp_diff*m.sin(m.pi*1/12*(hours-6-tmin))+((temp_max-temp_min)/2)*m.sin(m.pi*1/4320*(hours-2160))+(((temp_max-temp_min)/2)+temp_min)),2)
 
-def get_temperaturverlauf(months_start, months_end, days, temp_diff, temp_max, temp_min, tmin):
+def get_temperaturverlauf_haus(months_start, months_end, days, temp_diff, temp_max, temp_min, tmin):
     temperaturverlauf_haus = list()
     for hours in range(24*30*(months_start-1), 24*30*months_end+(24*days), 1): #24 Stunden * 30 Tage 
         temperaturverlauf_haus.append(temperaturverlauf_haus(hours, temp_diff, temp_max, temp_min, tmin))
@@ -15,4 +15,4 @@ def temperaturverlauf_keller(hours):
 def get_temperaturverlauf_keller (months_start, months_end, days)
     temperaturverlauf_keller = list()
     for hours in range ((24*30*(months_start-1), 24*30*months_end+(24*days), 1): #24 Stunden * 30 Tage 
-        temperaturvelruaf_keller.append(temperaturverlauf_keller(hours))
+        temperaturverlauf_keller.append(temperaturverlauf_keller(hours))
