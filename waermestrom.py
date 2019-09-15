037b846e71c9f0bb3fb39d84913f6fef8dccdcf0
SteffenTo <52286374+SteffenTo@users.noreply.github.com>
2019-09-15 16:01:41 +0200

    Update waermestrom.py
    Auslagern der Durchschnitts wärmestrom funktion

--- waermestrom.py
+++ waermestrom.py
@@ -1,23 +1,18 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jul 30 18:50:38 2019
-
-@author: sbeckmann
-"""
-
 import numpy as np
+
 
 def waermestromformel(temp_outside, temp_inside, r_lambda):
     if temp_outside >= temp_inside:
         return 0
     return np.divide(temp_outside - temp_inside, r_lambda)
 
+
 def calculate(list_of_temperatures_outside, r_lambda):
     waermestrom = list()
 
     for index, item in enumerate(list_of_temperatures_outside):
         hour = index % 24
-        
+
         if 10 <= hour and hour <= 18:
             temp_inside = 21
         else:
@@ -26,6 +21,14 @@ def calculate(list_of_temperatures_outside, r_lambda):
         waermestrom.append(waermestromformel(item, temp_inside, r_lambda))
     return waermestrom
 
-
+def waermestrom_durchschnitt_berechnung(months_start, months_end, waermestrom_stuendlich):
+    waermestrom_durchschnitt = list()
+    for month in range(months_end - months_start + 1):
+        # print("Monat: {}".format(month))
+        waermestromverlauf_this_month = waermestrom_stuendlich[month * (24 * 30): (month + 1) * 24 * 30]
+        waermestrom_durchschnitt.append(np.mean(waermestromverlauf_this_month))
+        # plt.plot(waermestromverlauf_this_month)
+        # plt.show()
+    return waermestrom_durchschnitt
 
 
