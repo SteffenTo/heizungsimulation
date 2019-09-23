f4a49c7ebda122822f3b305874a245bfcb3983fd
SteffenTo <52286374+SteffenTo@users.noreply.github.com>
2019-09-23 15:19:35 +0200

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,21 +1,27 @@
 import matplotlib.pyplot as plt
 import numpy as np
-def graph(waermestrom_durchschnitt, plot_days, waermestrom_stuendlich, name_jahr, name_tag):
-    plt.figure(figsize=(10, 5))
-    plt.plot(range(1, 13), np.absolute(waermestrom_durchschnitt))
-    plt.title(name_jahr)
+def graph(waermestrom_durchschnitt, ref_waermestrom_durchschnitt,  plot_days, waermestrom_stuendlich, ref_waermestrom_stuendlich):
+    plt.figure(figsize=(10, 5), num="Jahresverlauf")
+    plt.plot(range(1, 13), np.absolute(waermestrom_durchschnitt), label="Gewählte Dämmung")
+    plt.plot(range(1,13), np.absolute(ref_waermestrom_durchschnitt), label="Referenz ungedämmter Altbau")
+    plt.legend()
+    plt.title("Jahresverlauf")
     plt.xlabel("Monat")
     plt.ylabel("Durchschnittswärmestrom (in W(h))")
 
 
-    plt.figure(figsize=(10, 5))
+    plt.figure(figsize=(10, 5), num="Tagesverlauf")
 
     for (day, month) in plot_days:
         tageswaermestrom = waermestrom_stuendlich[
                            (day - 1) * 24 + (month - 1) * 24 * 30: day * 24 + (month - 1) * 24 * 30]
         plt.plot(np.absolute(tageswaermestrom), label=str(day) + "." + str(month))
+        ref_tageswaermestrom = ref_waermestrom_stuendlich[
+                               (day - 1) * 24 + (month - 1) * 24 * 30: day * 24 + (month - 1) * 24 * 30]
+        plt.plot(np.absolute(ref_tageswaermestrom), label="Referenz " + str(day) + "." + str(month))
+
     plt.legend()
     plt.ylabel("Wärmestrom (in W(h))")
     plt.xlabel("Uhrzeit (in Stunden)")
-    plt.title(name_tag)
+    plt.title("Tagesverlauf")
     plt.show()
