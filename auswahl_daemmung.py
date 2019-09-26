39d28ec467b8935997c6ca34f87393176b3cad00
SteffenTo <52286374+SteffenTo@users.noreply.github.com>
2019-09-26 19:38:59 +0200

    Update auswahl_daemmung.py

--- auswahl_daemmung.py
+++ auswahl_daemmung.py
@@ -7,11 +7,11 @@ print("Ein Haus mit bekannten Abmaßen soll gedämmt werden.\n"
       "oder den ungedämmten Altbau zu betrachten")
 
 
-#2. Verschleifung: Auswahl der möglichen Styropordicken, möglich für Wände/Dach/Boden
+# 2. Verschleifung: Auswahl der möglichen Styropordicken, möglich für Wände/Dach/Boden
 def s_styropor_wahl():
     if maßnahme == "a":
         s_styropor = 0.06
-        return (True, True, s_styropor) #return (allgemeinen Status, korrekte Eingabe, Styropordicke)
+        return (True, True, s_styropor)  # return (allgemeinen Status, korrekte Eingabe, Styropordicke)
 
     elif maßnahme == "b":
         s_styropor = 0.1
@@ -19,91 +19,91 @@ def s_styropor_wahl():
     elif maßnahme == "c":
         s_styropor = 0.15
         return (True, True, s_styropor)
-    else:   #für den Fall einer ungültigen Eingabe
+    else:  # für den Fall einer ungültigen Eingabe
         print("\nEingabe der Komponente ist ungültig. Bitte neue Komponente auswählen.\n")
         s_styropor = 0
         return (False, False, s_styropor)
 
 
-#2. Verschleifung: Auswahl der möglichen Verglasung, möglich für Fenster und Türen (immer zusammen)
+# 2. Verschleifung: Auswahl der möglichen Verglasung, möglich für Fenster und Türen (immer zusammen)
 def glas_wahl():
     if maßnahme == "a":
         s_fenster = 0.026
-        lambda_glas = 1
-        return (s_fenster, lambda_glas, True) #return (Glasdicke, Wärmeleitfähigkeit, korrekte Eingabe)
+        lambda_glas = 0.012
+        return (s_fenster, lambda_glas, True)  # return (Glasdicke, Wärmeleitfähigkeit, korrekte Eingabe)
 
     elif maßnahme == "b":
         s_fenster = 0.044
-        lambda_glas = 0.06
+        lambda_glas = 0.004
         return (s_fenster, lambda_glas, True)
 
-    else:   #für den Fall einer ungültigen Eingabe
+    else:  # für den Fall einer ungültigen Eingabe
         print("\nEingabe der Komponente ist ungültig. Bitte neue Komponente auswählen.\n")
         s_fenster = 0.004
-        lambda_glas = 5.6
+        lambda_glas = 0.024
         return (s_fenster, lambda_glas, False)
 
 
 def daemmung_wahl():
     global maßnahme
     korrekte_eingabe = False
-                                      
-    status_styropor_wand = False      #Festlegung der Werte für den ungedämmten Fall
-    status_styropor_boden = False        # es müssen nur Veränderungen berücksichtigt und ergänzt werden 
-    status_styropor_dach = False 
-    s_styropor = 0 
+
+    status_styropor_wand = False  # Festlegung der Werte für den ungedämmten Fall
+    status_styropor_boden = False  # es müssen nur Veränderungen berücksichtigt und ergänzt werden
+    status_styropor_dach = False
+    s_styropor = 0
     s_fenster = 0.004
-    lambda_glas = 5.6         
+    lambda_glas = 0.024
 
-    while korrekte_eingabe == False:    #solange diese Bedingung erfüllt ist, ist die Auswahl der Dämmung nicht
-                                            #abgeschlossen
-        #Legende: Gebäudekomponente
-        print ("Welche Gebäudekomponente soll gedämmt werden?\n 1 - Wände\n 2 - Dach\n 3 - Kellerboden\n "
-               "4 - Fenster und Türen\n 5 - alles\n 6 - Betrachtung des ungedämmten Altbaus\n "
-               "Bestätigen Sie alle Eingaben mithilfe der Enter-Taste")
+    while korrekte_eingabe == False:  # solange diese Bedingung erfüllt ist, ist die Auswahl der Dämmung nicht
+        # abgeschlossen
+        # Legende: Gebäudekomponente
+        print("Welche Gebäudekomponente soll gedämmt werden?\n 1 - Wände\n 2 - Dach\n 3 - Kellerboden\n "
+              "4 - Fenster und Türen\n 5 - alles\n 6 - Betrachtung des ungedämmten Altbaus\n "
+              "Bestätigen Sie alle Eingaben mithilfe der Enter-Taste")
 
-        komponente = input("Komponente: ")      #Eingabe der Komponente durch Nutzer entsprechend der Legende
+        komponente = input("Komponente: ")  # Eingabe der Komponente durch Nutzer entsprechend der Legende
 
         # 1. Verschleifung: Wahl der zu dämmenden Komponente
         if komponente == "1":
-            print ("\nDämmung der Wände durch Styroporschicht der Dicke \n a - 6cm\n b - 10cm\n c - 15cm")
-            maßnahme = input("Variante: ")      #Eingabe der Maßnahme durch Nutzer entsprechend der Legende,
-                                                    # springt in entsprechende 2. Schleife
-            (status_styropor_wand, korrekte_eingabe, s_styropor) = s_styropor_wahl()     #hier: 2.Schleife für die Wahl
-                                                                                            #des Styropors
+            print("\nDämmung der Wände durch Styroporschicht der Dicke \n a - 6cm\n b - 10cm\n c - 15cm")
+            maßnahme = input("Variante: ")  # Eingabe der Maßnahme durch Nutzer entsprechend der Legende,
+            # springt in entsprechende 2. Schleife
+            (status_styropor_wand, korrekte_eingabe, s_styropor) = s_styropor_wahl()  # hier: 2.Schleife für die Wahl
+            # des Styropors
 
         elif komponente == "2":
-            print ("\nDämmung des Dachs durch Styroporschicht der Dicke \n a - 6cm\n b - 10cm\n c - 15cm")
+            print("\nDämmung des Dachs durch Styroporschicht der Dicke \n a - 6cm\n b - 10cm\n c - 15cm")
             maßnahme = input("Variante: ")
             (status_styropor_dach, korrekte_eingabe, s_styropor) = s_styropor_wahl()
 
         elif komponente == "3":
-            print ("\nDämmung des Kellerboden durch Styroporschicht der Dicke \n a - 6cm\n b - 10cm\n c - 15cm")
+            print("\nDämmung des Kellerboden durch Styroporschicht der Dicke \n a - 6cm\n b - 10cm\n c - 15cm")
             maßnahme = input("Variante: ")
             (status_styropor_boden, korrekte_eingabe, s_styropor) = s_styropor_wahl()
 
-        elif komponente == "4":     #hier: 2.Schleife für die Wahl der Verglasung
+        elif komponente == "4":  # hier: 2.Schleife für die Wahl der Verglasung
             print("\nDämmung der Fenster und Türen durch\n a - Zweifach-Verbundglas\n b - Dreifach-Verbundglas")
             maßnahme = input("Variante: ")
             (s_fenster, lambda_glas, korrekte_eingabe) = glas_wahl()
 
-        elif komponente == "5":     #keine 2. Schleife nötig, da alle notwendigen Informationen bekannt
+        elif komponente == "5":  # keine 2. Schleife nötig, da alle notwendigen Informationen bekannt
             print("\nEs wird eine Dämmung sämtlicher Komponenten durch 15cm dickes Styropor und Dreifachverbundglas "
                   "vorgenommen")
 
             s_fenster = 0.044
-            lambda_glas = 0.06
+            lambda_glas = 0.004
             s_styropor = 0.15
             status_styropor_boden = True
             status_styropor_wand = True
             status_styropor_dach = True
             korrekte_eingabe = True
 
-        elif komponente == "6":    #keine 2. Schleife nötig, da alle notwendigen Informationen bereits zu Beginn bekannt
+        elif komponente == "6":  # keine 2. Schleife nötig, da alle notwendigen Informationen bereits zu Beginn bekannt
             korrekte_eingabe = True
             print("\nEs wird der Wärmestrom des ungedämmten Altbaus betrachtet")
 
-        else:   #für den Fall einer ungültigen Eingabe
+        else:  # für den Fall einer ungültigen Eingabe
             print("\nEingabe der Komponente ist ungültig. Bitte neue Komponente auswählen.\n")
     return (status_styropor_wand, status_styropor_boden, status_styropor_dach, s_styropor, s_fenster, lambda_glas)
 
@@ -135,17 +135,17 @@ def soll_temperatur():
 
             print("\nGeben Sie an, welche Innentemperatur nachts angestrebt wird."
                   "Die Wahl muss zwischen 16 und 19°C liegen.")
-            while korrekt_nacht  == False:
+            while korrekt_nacht == False:
                 t_nacht = float(input("Eingabe: "))
                 if 16 <= t_nacht <= 19:
-                    korrekt_nacht  = True
+                    korrekt_nacht = True
                 else:
                     print("Diese Temperatur ist unzulässig. Bitte tätigen Sie eine gültige Eingabe.")
 
             print("\nGeben Sie an, ab welchem Zeitpunkt, die Tagestemperatur vorliegen soll. "
                   "Dieser muss zwischen 5 und 9 Uhr morgens liegen")
             while korrekt_t1 == False:
-                t1= float(input("Eingabe: "))
+                t1 = float(input("Eingabe: "))
                 if 5 <= t1 <= 9:
                     korrekt_t1 = True
                 else:
@@ -154,7 +154,7 @@ def soll_temperatur():
             print("\nGeben Sie an, ab welchem Zeitpunkt, die Nachttemperatur vorliegen soll. "
                   "Dieser muss zwischen 20 und 23 Uhr abends liegen")
             while korrekt_t2 == False:
-                t2= float(input("Eingabe: "))
+                t2 = float(input("Eingabe: "))
                 if 20 <= t2 <= 23:
                     korrekt_t2 = True
                 else:
