24dbc12d28c668404472827c6d25501bd9a406cf
SteffenTo <52286374+SteffenTo@users.noreply.github.com>
2019-10-02 16:33:42 +0200

    Update waermestrom.py

--- waermestrom.py
+++ waermestrom.py
@@ -4,10 +4,10 @@ import numpy as np
 def waermestromformel(temp_outside, temp_inside, r_lambda):
     if temp_outside >= temp_inside:
         return 0
-    return (np.divide(temp_outside - temp_inside, r_lambda))/1000           #/1000 um die Werte direkt in kW zu haben
+    return (np.divide(temp_outside - temp_inside, r_lambda))/1000
 
 
-def calculate(list_of_temperatures_outside, r_lambda, t_tag, t_nacht, t1, t2):
+def berechnung(list_of_temperatures_outside, r_lambda, t_tag, t_nacht, t1, t2):
     waermestrom = list()
 
     for index, item in enumerate(list_of_temperatures_outside):
@@ -21,17 +21,11 @@ def calculate(list_of_temperatures_outside, r_lambda, t_tag, t_nacht, t1, t2):
         waermestrom.append(waermestromformel(item, temp_inside, r_lambda))
     return waermestrom
 
-    waermestrom.append(waermestromformel(item, temp_inside, r_lambda))
-    return waermestrom
-
 def waermestrom_durchschnitt_berechnung(months_start, months_end, waermestrom_stuendlich):
     waermestrom_durchschnitt = list()
     for month in range(months_end - months_start + 1):
-        # print("Monat: {}".format(month))
         waermestromverlauf_this_month = waermestrom_stuendlich[month * (24 * 30): (month + 1) * 24 * 30]
         waermestrom_durchschnitt.append(np.mean(waermestromverlauf_this_month))
-        # plt.plot(waermestromverlauf_this_month)
-        # plt.show()
     return waermestrom_durchschnitt
 
 
