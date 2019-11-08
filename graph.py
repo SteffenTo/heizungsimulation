12f695352e91b71ae6b93e7c6e8a0b152f8e2a08
SteffenTo <52286374+SteffenTo@users.noreply.github.com>
2019-11-08 10:21:30 +0100

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,13 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def graph(waermestrom_durchschnitt, ref_waermestrom_durchschnitt,  daten_drucken, daten_original, waermestrom_stuendlich,
-          ref_waermestrom_stuendlich, temperaturverlauf_aussen, temperatur_soll, temperatur_ist):
 
+def graph(waermestrom_durchschnitt, ref_waermestrom_durchschnitt, daten_drucken, daten_original, waermestrom_stuendlich,
+          ref_waermestrom_stuendlich, temperaturverlauf_aussen, temperatur_soll, temperatur_ist):
     # Temperaturverlauf
     zaehler = 0
     for i in daten_original:
-        plt.figure(figsize=(11, 6), num = str(i))
+        plt.figure(figsize=(11, 6), num=str(i))
         plt.grid()
         plt.xticks(np.arange(0, 25, 1.0))
         plt.xlim(0, 24)
@@ -17,10 +17,10 @@ def graph(waermestrom_durchschnitt, ref_waermestrom_durchschnitt,  daten_drucken
         day, month = daten_drucken[zaehler]
         zaehler += 1
         tagestemperaturverlauf = temperaturverlauf_aussen[
-                                 (day - 1) * 24 + (month - 1) * 24 * 30: (day * 24 + (month - 1) * 24 * 30)+1]
-        plt.plot(range(0, 25), tagestemperaturverlauf, label = "Außentemperatur")
-        plt.plot(np.arange(0, 25, 0.25), temperatur_soll, label = "Sollinnentemperatur")
-        plt.plot(np.arange(0, 25, 0.25), temperatur_ist, label = "Istinnentemperatur")
+                                 (day - 1) * 24 + (month - 1) * 24 * 30: (day * 24 + (month - 1) * 24 * 30) + 1]
+        plt.plot(range(0, 25), tagestemperaturverlauf, label="Außentemperatur")
+        plt.plot(np.arange(0, 25, 0.25), temperatur_soll, label="Sollinnentemperatur")
+        plt.plot(np.arange(0, 25, 0.25), temperatur_ist, label="Istinnentemperatur")
         plt.legend()
 
     # Tagesverbrauch
@@ -39,8 +39,8 @@ def graph(waermestrom_durchschnitt, ref_waermestrom_durchschnitt,  daten_drucken
         plt.plot(range(0, 25), np.absolute(tageswaermestrom), farbe[index], label=str(day) + "." + str(month))
         ref_tageswaermestrom = ref_waermestrom_stuendlich[
                                (day - 1) * 24 + (month - 1) * 24 * 30: (day * 24 + (month - 1) * 24 * 30) + 1]
-        plt.plot(range(0, 25), np.absolute(ref_tageswaermestrom), farbe[index], label=str(day) + "." + str(month) + " Referenz",
-                 linestyle='dotted')
+        plt.plot(range(0, 25), np.absolute(ref_tageswaermestrom), farbe[index], label=str(day) + "." + str(month) +
+                                                                                      " Referenz", linestyle='dotted')
         index += 1
     plt.legend()
 
@@ -54,7 +54,8 @@ def graph(waermestrom_durchschnitt, ref_waermestrom_durchschnitt,  daten_drucken
     plt.title("Jahresverlauf")
     plt.ylabel("Durchschnittsenergieverbrauch in kWh")
     plt.plot(range(1, 13), np.absolute(waermestrom_durchschnitt), label="Gewählte Dämmung")
-    plt.plot(range(1, 13), np.absolute(ref_waermestrom_durchschnitt), label="Referenz ungedämmter Altbau", linestyle = 'dotted')
+    plt.plot(range(1, 13), np.absolute(ref_waermestrom_durchschnitt), label="Referenz ungedämmter Altbau",
+                                                                                        linestyle='dotted')
     plt.legend()
 
     plt.show()
